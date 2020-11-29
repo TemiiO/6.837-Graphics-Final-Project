bf0c3fd328d8c8942a28db0849ad588c2a2c7bc0
RicardoGayle <rgayle@mit.edu>
2020-11-29 02:39:29 -0500

    Added necessary code from PSet 3

--- project.py
+++
@@ -1,3 +0,0 @@
-#Final Project for 6.837 by Temi Omitoogun and Ricardo Gayle
-for i in range(int(6.837)):
-    print("Let's get this going")
---
+++ project_code/IntegratorFactory.hpp
@@ -0,0 +1,32 @@
+#ifndef INTEGRATOR_FACTORY_H_
+#define INTEGRATOR_FACTORY_H_
+
+#include "IntegratorBase.hpp"
+
+#include <stdexcept>
+
+#include "gloo/utils.hpp"
+
+#include "IntegratorType.hpp"
+#include "ForwardEulerIntegrator.hpp"
+#include "TrapezoidIntegrator.hpp"
+#include "Rk4Integrator.hpp"
+
+namespace GLOO {
+class IntegratorFactory {
+ public:
+  template <class TSystem, class TState>
+  static std::unique_ptr<IntegratorBase<TSystem, TState>> CreateIntegrator(IntegratorType type) {
+        if (type == IntegratorType::Euler) {
+          return make_unique<ForwardEulerIntegrator<TSystem, TState>>();
+        } else if (type == IntegratorType::Trapezoidal) {
+          return make_unique<TrapezoidIntegrator<TSystem, TState>>();
+        } else if (type == IntegratorType::RK4) {
+          return make_unique<Rk4Integrator<TSystem, TState>>();
+        }
+        return nullptr;
+  }
+};
+}  // namespace GLOO
+
+#endif
