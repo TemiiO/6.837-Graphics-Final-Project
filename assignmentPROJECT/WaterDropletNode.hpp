e2b66e7a23923a9df27b686a78526ca3875d1046
RicardoGayle <rgayle@mit.edu>
2020-11-29 19:01:09 -0500

    Finished WaterDropletNode.hpp

--- assignmentPROJECT/WaterDropletNode.hpp
+++ assignmentPROJECT/WaterDropletNode.hpp
@@ -12,6 +12,22 @@
 #include "IntegratorType.hpp"
 #include "IntegratorFactory.hpp"
 
+namespace GLOO {
+    class WaterDropletNode : public SceneNode {
+        public:
+            WaterDropletNode(IntegratorType type, flaot integration_step);
 
+        private:
+            std::shared_ptr<VertexObject> particle_mesh_;
+            ParticleState state_;
+            # TODO Add WaterDropletSystem
+            std::shared_ptr<ShaderProgram> shader_;
+            SceneNode* particle_;
+            std::unique_ptr<GLOO::IntegratorBase<WaterDropletSystem, ParticleState>> integrator_;
+            float integration_step_;
+            double time_;
 
+            void Update(double dt);
+    };
+}
 #endif
